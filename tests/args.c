5ad5bf775f05482db9a45fb38276ffa3944c5a7e
Eric Radman <ericshane@eradman.com>
2018-08-27 22:18:14 -0400

    0.5 release

--- tests/args.c
+++ tests/args.c
@@ -8,6 +8,7 @@
 
 int
 main(int argc, char *argv[]) {
+	int i;
 	int cmd_argc;
 	char *cmd_argv[32];
 
@@ -16,7 +17,7 @@ main(int argc, char *argv[]) {
 	cmd_argc = append(cmd_argv, cmd_argc, "ssh", "-fnNT", "-R", "6000:localhost:65321", "-S", NULL);
 	append(cmd_argv, cmd_argc, "/tmp/rset_control_172.16.0.5", "-M", "172.16.0.5", NULL);
 	run(cmd_argv);
-	for (int i=0; cmd_argv[i]; i++)
+	for (i=0; cmd_argv[i]; i++)
 		printf("%d: %s\n", i, cmd_argv[i]);
 
 	return 0;
