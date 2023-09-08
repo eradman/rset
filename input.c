8482f28016e66bdfc6462b6b89d9c94893907174
Omar Polo <op@omarpolo.com>
2023-09-08 14:58:15 +0200

    fix mkstemp(3) usage (#31)
    unlike mktemp(3), mkstemp returns the file descriptor so no need
    to open it again.

--- input.c
+++ input.c
@@ -81,8 +81,7 @@ yylex() {
 				exit(1);
 			}
 			strlcpy(tmp_src, "/tmp/rset_local.XXXXXX", sizeof tmp_src);
-			mkstemp(tmp_src);
-			if ((tfd = open(tmp_src, O_CREAT|O_RDWR, 0600)) == -1)
+			if ((tfd = mkstemp(tmp_src)) == -1)
 				err(1, "open %s", tmp_src);
 		}
 
