196a6a6e01a45389bd374fd9cfa8e812eaece4ff
Eric Radman <ericshane@eradman.com>
2023-11-07 13:14:05 -0500

    Ensure final.env is generated for each hostname

--- execute.c
+++ execute.c
@@ -316,15 +316,18 @@ update_environment_file(char *host_name, char *socket_path, Label *host_label, i
 	Options op;
 	static char environment_set[PLN_OPTION_SIZE] = "";
 	static char environment_file_set[PLN_OPTION_SIZE] = "";
+	static char host_name_set[PLN_LABEL_SIZE] = "";
 
 	apply_default(op.environment, host_label->options.environment, ENVIRONMENT);
 	apply_default(op.environment_file, host_label->options.environment_file, ENVIRONMENT_FILE);
 
 	/* only update when value changes */
-	if ((strcmp(environment_set, op.environment) == 0) &&
+	if ((strcmp(host_name_set, host_name) == 0) &&
+	    (strcmp(environment_set, op.environment) == 0) &&
 	    (strcmp(environment_file_set, op.environment_file) == 0))
 		return 0;
 
+	strlcpy(host_name_set, host_name, PLN_OPTION_SIZE);
 	len = strlcpy(environment_set, op.environment, PLN_OPTION_SIZE);
 	strlcpy(environment_file_set, op.environment_file, PLN_OPTION_SIZE);
 
