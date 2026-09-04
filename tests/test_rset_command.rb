e5547e113c6738ad1fd3ac70d97adb982ea6bf77
Eric Radman <ericshane@eradman.com>
2026-09-04 19:56:46 -0400

    Change command 'shell-env' to '-- wenv'
    Anticipate additional commands and add test runner.  Include minimal
    error handling for incorrect number of arguments. Add usage to '-h'
    summary.

---
+++ tests/test_rset_command.rb
@@ -0,0 +1,61 @@
+require 'open3'
+require 'tempfile'
+
+# Test Utilities
+@tests = 0
+@test_description = 0
+
+# Setup
+@systmp = Dir.mktmpdir
+
+at_exit do
+  FileUtils.remove_dir @systmp
+end
+
+ENV['PATH'] = "#{Dir.pwd}/../:#{ENV.fetch('PATH', nil)}"
+
+def try(descr)
+  start = Time.now
+  @tests += 1
+  @test_description = descr
+  yield
+  delta = format('%.3f', Time.now - start)
+  delta = "\e[37m#{delta}\e[39m"
+  puts "#{delta}: #{descr}"
+end
+
+def eq(result, expected)
+  a = result.to_s.gsub(/^/, '> ')
+  b = expected.to_s.gsub(/^/, '< ')
+  raise "\"#{@test_description}\"\n#{a}\n#{b}" unless result == expected
+end
+
+puts "\e[32m---\e[39m"
+
+# Usage test
+
+try 'Missing command' do
+  cmd = '../rset --'
+  _, err, status = Open3.capture3(cmd)
+  eq err.include?('usage: rset'), true
+  eq status.success?, false
+end
+
+try 'Invalid command' do
+  ['shell', 'wenv wenv'].each do |option|
+    cmd = "../rset -- #{option}"
+    _, err, status = Open3.capture3(cmd)
+    eq err.include?('rset: unknown command:'), true
+    eq status.success?, false
+  end
+end
+
+# Commands
+
+try 'Print shell environment' do
+  cmd = '../rset -- wenv'
+  out, _, status = Open3.capture3(cmd)
+  eq out.include?('export RSET_HOST_CONNECT='), true
+  eq out.include?('export RSET_HOST_DISCONNECT='), true
+  eq status.success?, true
+end
